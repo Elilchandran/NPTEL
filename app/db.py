0b073370fd9338d525ec3e63e73b3fb0fe357002
Elilchandran <elilchandran@gmail.com>
2024-10-27 10:46:06 +0530

    final update

--- app/db.py
+++ app/db.py
@@ -1,3 +1,26 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+def create_connection():
+    try:
+        db_url = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+        print(f"Connecting to database with URL: {db_url}")
+        engine = create_engine(db_url)
+        connection = engine.connect() 
+        print("Database connection successful.")
+        connection.close() 
+        return engine
+    except SQLAlchemyError as e:
+        print(f"Error creating engine: {e}")
+        return None
+
+
+
+
 # import os
 # from sqlalchemy import create_engine
 # from sqlalchemy.exc import SQLAlchemyError
@@ -14,22 +37,4 @@
 #     except SQLAlchemyError as e:
 #         print(f"Error creating engine: {e}")
 #         return None
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.exc import SQLAlchemyError
-from dotenv import load_dotenv
 
-load_dotenv() 
-
-def create_connection():
-    try:
-        db_url = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-        print(f"Connecting to database with URL: {db_url}")
-        engine = create_engine(db_url)
-        connection = engine.connect()  # Attempt to connect to the database
-        print("Database connection successful.")
-        connection.close()  # Close after testing
-        return engine
-    except SQLAlchemyError as e:
-        print(f"Error creating engine: {e}")
-        return None
--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from flask import Flask, render_template
+from app.queries import fetch_course_registration_data
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    df = fetch_course_registration_data()
+    data = df.to_dict(orient='records') if df is not None else []
+    return render_template('index.html', data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 # from flask import Flask, render_template
 # from app.queries import fetch_course_registration_data
 # import os
@@ -14,17 +29,4 @@
 # if __name__ == "__main__":
 #     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)), debug=True)
 
-from flask import Flask, render_template
-from app.queries import fetch_course_registration_data
 
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    df = fetch_course_registration_data()
-    data = df.to_dict(orient='records') if df is not None else []
-    return render_template('index.html', data=data)
-
-if __name__ == "__main__":
-    app.run(debug=True)
